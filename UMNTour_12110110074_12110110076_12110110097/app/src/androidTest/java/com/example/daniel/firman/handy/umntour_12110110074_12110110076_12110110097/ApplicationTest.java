df153c3591a2ec8f60261336abce746c8d57419b
Handy Wijaya <handy.wijaya.p@gmail.com>
2015-11-26 12:07:31 +0700

    init

---
+++ UMNTour_12110110074_12110110076_12110110097/app/src/androidTest/java/com/example/daniel/firman/handy/umntour_12110110074_12110110076_12110110097/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.daniel.firman.handy.umntour_12110110074_12110110076_12110110097;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
